afc2bef2a3490b526fe7ea106919f0289bb4dd07
JohnGirma <yohannesgirma543@gmial.com>
2023-10-02 17:44:27 +0300

    update

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import './App.css'
 import NavBar from './components/NavBar'
+import Footer from './components/Footer'
 function App() {
 
   return (
@@ -8,6 +9,7 @@ function App() {
      <h1 className="text-6xl font-bold underline">
       Hello world!
     </h1>
+    <Footer/>
     </>
   )
 }
