6148e96d6560632f1ae6086e591cff70db056300
JohnGirma <yohannesgirma543@gmial.com>
2023-10-02 11:28:00 +0300

    setup tailwindcss

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,13 @@
 import './App.css'
-
+import NavBar from './components/NavBar'
 function App() {
 
   return (
     <>
-     <h1>hello world</h1>
+    {/* <NavBar/> */}
+     <h1 className="text-6xl font-bold underline">
+      Hello world!
+    </h1>
     </>
   )
 }
---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      colors:{
+        primary:"#413C58"
+      }
+    },
+  },
+  plugins: [],
+}
