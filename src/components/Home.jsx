04ef09f0d2e2b29ad74041f1061a48f673229a3c
JohnGirma <yohannesgirma543@gmial.com>
2023-10-16 12:55:12 +0300

    aligni tems

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,20 +1,17 @@
 import React from 'react'
-import NavBar from './NavBar'
-
 import Firstcnc from '../assets/Firstcnc.png'
 
 
 function Home() {
   return (
     <>
-    <NavBar/>
    <div className="container mx-auto flex flex-wrap justify-around bg-white h-screen">
     <div className="flex flex-col justify-center items-start gap-6">
-        <h1 className="font-bold text-7xl text-center">Welcome to </h1>
-        <h1 className="font-bold text-7xl  text-center">Markon Tech!</h1>
-        <p className="text-xl text-center">Cut-N-Create</p>
-        <button className="px-4 py-2 mt-4 bg-black text-white font-semibold">BUY</button>
-    </div>
+        <h1 className="font-bold text-7xl text-center font-['Poppins'] ">Welcome to </h1>
+        <h1 className="font-bold text-7xl  text-center font-['Poppins'] ">Markon Tech!</h1>
+        <p className=" text-stone-400 text-4xl font-semibold font-['Open Sans'] text-center">Cut-N-Create</p>
+       <button className="w-40 h-14 px-2 py-4mt-4 bg-black text-white font-semibold font-['Inter'] rounded-md">BUY</button>
+     </div>
     <img className="h-96 justify-center items-center self-center" src={Firstcnc} alt="First CNC" />
 </div>
 
