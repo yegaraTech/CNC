08a1cefc0c37994c65c32214af2bd1a30e8cf7a3
JohnGirma <yohannesgirma543@gmial.com>
2023-10-21 13:47:30 +0300

    add the block

---
+++ fefef.js
@@ -0,0 +1,260 @@
+<div className="w-96 h-96 relative bg-white">
+Who We Are
+  <img className="w-96 h-96 left-[682px] top-[222px] absolute" src="https://via.placeholder.com/666x594" />
+  <div className="left-[140px] top-[278px] absolute flex-col justify-start items-start gap-10 inline-flex">
+    <div className="flex-col justify-start items-start gap-6 flex">
+      <div className="w-96 text-neutral-800 text-6xl font-bold font-['Poppins'] leading-10">Welcome to Markon  Tech!</div>
+      <div className="text-center text-stone-400 text-4xl font-semibold font-['Open Sans']">Cut-N-Create</div>
+    </div>
+    <div className="w-40 h-14 px-2 py-4 bg-neutral-900 rounded-md justify-center items-center inline-flex">
+      <div className="text-white text-xl font-semibold font-['Inter']">BUY</div>
+    </div>
+  </div>
+  <div className="w-96 left-[140px] top-[3px] absolute justify-start items-center gap-36 inline-flex">
+    <div className="h-20 pl-24 justify-end items-center gap-px flex">
+      <div className="pr-2.5 justify-end items-center flex">
+        <div className="flex-col justify-start items-start inline-flex">
+          <div className="self-stretch flex-col justify-start items-start flex">
+            <div className="justify-start items-center inline-flex">
+              <div className="justify-start items-start flex">
+                <div className="self-stretch flex-col justify-center items-start inline-flex">
+                  <div className="self-stretch px-3.5 justify-start items-center inline-flex">
+                    <div className="flex-col justify-start items-start inline-flex">
+                      <div className="text-neutral-700 text-base font-medium font-['Roboto'] leading-10">HOME</div>
+                    </div>
+                    <div className="pl-2.5 py-9" />
+                  </div>
+                </div>
+                <div className="self-stretch flex-col justify-center items-start inline-flex">
+                  <div className="self-stretch px-3.5 justify-start items-center inline-flex">
+                    <div className="flex-col justify-start items-start inline-flex">
+                      <div className="text-neutral-700 text-base font-medium font-['Roboto'] leading-10">ABOUT US</div>
+                    </div>
+                    <div className="pl-2.5 py-9" />
+                  </div>
+                </div>
+                <div className="self-stretch flex-col justify-center items-start inline-flex">
+                  <div className="self-stretch px-3.5 justify-start items-center inline-flex">
+                    <div className="flex-col justify-start items-start inline-flex">
+                      <div className="text-neutral-700 text-base font-medium font-['Roboto'] leading-10">SERVICES</div>
+                    </div>
+                    <div className="pl-2.5 py-9" />
+                  </div>
+                </div>
+                <div className="self-stretch flex-col justify-center items-start inline-flex">
+                  <div className="px-3.5 justify-start items-center inline-flex">
+                    <div className="flex-col justify-start items-start inline-flex">
+                      <div className="text-neutral-700 text-base font-medium font-['Roboto'] leading-10">RESOURCES</div>
+                    </div>
+                    <div className="pl-2.5 py-9 flex-col justify-start items-start inline-flex">
+                      <div className="w-2 h-2 relative" />
+                    </div>
+                  </div>
+                </div>
+                <div className="self-stretch flex-col justify-center items-start inline-flex">
+                  <div className="self-stretch px-3.5 justify-start items-center inline-flex">
+                    <div className="flex-col justify-start items-start inline-flex">
+                      <div className="text-neutral-700 text-base font-medium font-['Roboto'] leading-10">COMMUNITY</div>
+                    </div>
+                    <div className="pl-2.5 py-9 flex-col justify-start items-start inline-flex">
+                      <div className="w-2 h-2 relative" />
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className="pl-2.5 rounded-2xl justify-start items-center flex">
+        <div className="flex-col justify-start items-start inline-flex">
+          <div className="justify-start items-start inline-flex">
+            <div className="px-10 py-2.5 bg-neutral-800 rounded-sm flex-col justify-start items-start inline-flex">
+              <div className="text-white text-sm font-medium font-['Roboto'] leading-none">Contact Us</div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+  <div className="w-96 h-96 left-0 top-[803px] absolute bg-stone-50" />
+  <div className="w-96 h-96 left-[-452px] top-[2077.04px] absolute origin-top-left rotate-[-74.57deg] bg-black bg-opacity-20" />
+  <div className="w-96 h-72 left-[140px] top-[1221px] absolute text-justify text-zinc-600 text-3xl font-light font-['Open Sans'] leading-10">At Markon-Tech, we specialize in the production of cutting-edge laser and router CNC machines. Our passion for precision engineering drives us to deliver top-tier solutions for your manufacturing needs. With a commitment to innovation and excellence, we're dedicated to providing you with the tools that empower your creativity and elevate your craftsmanship.
+  </div>
+  <div className="w-96 h-20 left-[464px] top-[1059px] absolute text-center text-neutral-800 text-6xl font-normal font-['Quando'] leading-10">Who We Are</div>
+  <div className="w-96 h-56 left-[771px] top-[6835px] absolute">
+    <div className="w-96 h-56 left-0 top-0 absolute bg-stone-50 rounded-2xl" />
+    <div className="w-96 left-[30px] top-[30px] absolute text-zinc-600 text-base font-normal font-['Open Sans'] leading-relaxed">Lorem ipsum dolor sit amet consectetur. Condimentum eget vitae ligula sed urna sit sagittis interdum a. Blandit mattis mattis lobortis orci. Facilisis dui sagittis tempor egestas pellentesque eu maecenas. Risus lectus nisl.</div>
+    <img className="w-12 h-12 left-[30px] top-[154px] absolute rounded-full" src="https://via.placeholder.com/50x50" />
+    <div className="w-36 h-12 left-[94px] top-[155px] absolute rounded-2xl flex-col justify-start items-start gap-1 inline-flex">
+      <div className="text-black text-base font-normal font-['Poppins']">Jacqueline Wright</div>
+      <div className="text-neutral-400 text-sm font-normal font-['Open Sans']">Customer</div>
+    </div>
+  </div>
+  <div className="w-96 h-56 left-[149px] top-[6835px] absolute">
+    <div className="w-96 h-56 left-0 top-0 absolute bg-stone-50 rounded-2xl" />
+    <div className="w-96 left-[30px] top-[30px] absolute text-zinc-600 text-base font-normal font-['Open Sans'] leading-relaxed">Lorem ipsum dolor sit amet consectetur. Condimentum eget vitae ligula sed urna sit sagittis interdum a. Blandit mattis mattis lobortis orci. Facilisis dui sagittis tempor egestas pellentesque eu maecenas. Risus lectus nisl.</div>
+    <img className="w-12 h-12 left-[30px] top-[154px] absolute rounded-full" src="https://via.placeholder.com/50x50" />
+    <div className="w-36 h-12 left-[94px] top-[155px] absolute flex-col justify-start items-start gap-1 inline-flex">
+      <div className="text-black text-base font-normal font-['Poppins']">Jacqueline Wright</div>
+      <div className="text-neutral-400 text-sm font-normal font-['Open Sans']">Customer</div>
+    </div>
+  </div>
+  <div className="w-96 h-56 left-[771px] top-[7154px] absolute">
+    <div className="w-96 h-56 left-0 top-0 absolute bg-stone-50 rounded-2xl" />
+    <div className="w-96 left-[30px] top-[30px] absolute text-zinc-600 text-base font-normal font-['Open Sans'] leading-relaxed">Lorem ipsum dolor sit amet consectetur. Condimentum eget vitae ligula sed urna sit sagittis interdum a. Blandit mattis mattis lobortis orci. Facilisis dui sagittis tempor egestas pellentesque eu maecenas. Risus lectus nisl.</div>
+    <img className="w-12 h-12 left-[30px] top-[154px] absolute rounded-full" src="https://via.placeholder.com/50x50" />
+    <div className="w-36 h-12 left-[94px] top-[155px] absolute rounded-2xl flex-col justify-start items-start gap-1 inline-flex">
+      <div className="text-black text-base font-normal font-['Poppins']">Jacqueline Wright</div>
+      <div className="text-neutral-400 text-sm font-normal font-['Open Sans']">Customer</div>
+    </div>
+  </div>
+  <div className="w-96 h-56 left-[149px] top-[7154px] absolute">
+    <div className="w-96 h-56 left-0 top-0 absolute bg-stone-50 rounded-2xl" />
+    <div className="w-96 left-[30px] top-[30px] absolute text-zinc-600 text-base font-normal font-['Open Sans'] leading-relaxed">Lorem ipsum dolor sit amet consectetur. Condimentum eget vitae ligula sed urna sit sagittis interdum a. Blandit mattis mattis lobortis orci. Facilisis dui sagittis tempor egestas pellentesque eu maecenas. Risus lectus nisl.</div>
+    <img className="w-12 h-12 left-[30px] top-[154px] absolute rounded-full" src="https://via.placeholder.com/50x50" />
+    <div className="w-36 h-12 left-[94px] top-[155px] absolute rounded-2xl flex-col justify-start items-start gap-1 inline-flex">
+      <div className="text-black text-base font-normal font-['Poppins']">Jacqueline Wright</div>
+      <div className="text-neutral-400 text-sm font-normal font-['Open Sans']">Customer</div>
+    </div>
+  </div>
+  <div className="origin-top-left rotate-180 left-[176px] top-[4367px] absolute">
+    <div className="w-9 h-9 left-0 top-0 absolute origin-top-left rotate-180 bg-stone-50 rounded-full" />
+  </div>
+  <div className="w-96 h-96 left-0 top-[7742px] absolute bg-stone-50" />
+  <div className="w-20 h-44 left-[636px] top-[7865px] absolute">
+    <div className="left-0 top-0 absolute text-neutral-800 text-xl font-medium font-['Poppins']">Heading</div>
+    <div className="left-0 top-[47px] absolute opacity-60 text-zinc-800 text-base font-normal font-['Open Sans']">Link here</div>
+    <div className="left-0 top-[81px] absolute opacity-60 text-zinc-800 text-base font-normal font-['Open Sans']">Link here</div>
+    <div className="left-0 top-[115px] absolute opacity-60 text-zinc-800 text-base font-normal font-['Open Sans']">Link here</div>
+    <div className="left-0 top-[149px] absolute opacity-60 text-zinc-800 text-base font-normal font-['Open Sans']">Link here</div>
+  </div>
+  <div className="w-20 h-44 left-[835px] top-[7865px] absolute">
+    <div className="left-0 top-0 absolute text-neutral-800 text-xl font-medium font-['Poppins']">Heading</div>
+    <div className="left-0 top-[47px] absolute opacity-60 text-zinc-800 text-base font-normal font-['Open Sans']">Link here</div>
+    <div className="left-0 top-[81px] absolute opacity-60 text-zinc-800 text-base font-normal font-['Open Sans']">Link here</div>
+    <div className="left-0 top-[115px] absolute opacity-60 text-zinc-800 text-base font-normal font-['Open Sans']">Link here</div>
+    <div className="left-0 top-[149px] absolute opacity-60 text-zinc-800 text-base font-normal font-['Open Sans']">Link here</div>
+  </div>
+  <div className="left-[1060px] top-[8425px] absolute text-neutral-800 text-xl font-medium font-['Poppins']">Connect with us</div>
+  <div className="left-[173px] top-[8409px] absolute" />
+  <img className="w-48 h-40 left-[230px] top-[8408px] absolute" src="https://via.placeholder.com/187x158" />
+  <img className="w-96 h-96 left-0 top-[7579px] absolute" src="https://via.placeholder.com/1442x790" />
+  <div className="left-[474px] top-[6715px] absolute text-center text-neutral-800 text-5xl font-normal font-['Preahvihear']">Our Happy Clients</div>
+  <div className="w-96 px-12 py-7 left-[142px] top-[7528px] absolute bg-neutral-800 rounded-lg justify-between items-center inline-flex">
+    <div className="flex-col justify-center items-start gap-2.5 inline-flex">
+      <div className="text-center text-white text-3xl font-medium font-['Poppins']">Need more information?</div>
+      <div className="text-center text-white text-xl font-normal font-['Open Sans']">Write your concern to us and our specialist will get back to you.</div>
+    </div>
+    <div className="w-40 h-14 px-2 py-4 bg-white rounded-lg justify-center items-center flex">
+      <div className="text-neutral-800 text-2xl font-semibold font-['Inter']">Click Here</div>
+    </div>
+  </div>
+  <div className="left-[1072px] top-[8476px] absolute justify-start items-start gap-4 inline-flex" />
+  <div className="w-96 left-[150px] top-[700px] absolute bg-white rounded-lg shadow justify-start items-start inline-flex">
+    <div className="w-72 h-52 px-2.5 justify-start items-start flex">
+      <div className="w-64 h-52 relative rounded-tr-sm">
+        <div className="h-16 pb-5 left-[10px] top-[24px] absolute flex-col justify-start items-start inline-flex">
+          <div className="self-stretch h-12 px-24 pb-2.5 flex-col justify-start items-center flex">
+            <div className="justify-start items-start inline-flex">
+              <div className="w-10 h-10 px-0.5 justify-center items-center flex">
+                <div className="grow shrink basis-0 self-stretch pl-px flex-col justify-start items-center inline-flex">
+                  <div className="text-center text-green-600 text-opacity-50 text-4xl font-normal font-['Font Awesome 5 Free'] leading-10"></div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className="h-16 pb-11 left-[10px] top-[79.84px] absolute flex-col justify-start items-start inline-flex">
+          <div className="self-stretch h-7 px-20 flex-col justify-start items-center flex">
+            <div className="text-center text-black text-base font-bold font-['Roboto'] leading-7">Flexible MOQ</div>
+          </div>
+        </div>
+        <div className="h-12 pb-6 left-[10px] top-[121.69px] absolute flex-col justify-start items-start inline-flex">
+          <div className="self-stretch h-6 pr-6 flex-col justify-start items-start flex">
+            <div className="text-neutral-700 text-sm font-normal font-['Roboto'] leading-relaxed">from 1 to 10,000 as per your needs.</div>
+          </div>
+        </div>
+      </div>
+    </div>
+    <div className="w-72 h-52 px-2.5 justify-start items-start flex">
+      <div className="w-64 h-52 relative rounded-tr-sm">
+        <div className="h-16 pb-5 left-[10px] top-[24px] absolute flex-col justify-start items-start inline-flex">
+          <div className="self-stretch h-12 px-24 pb-2.5 flex-col justify-start items-center flex">
+            <div className="justify-start items-start inline-flex">
+              <div className="w-10 h-10 justify-center items-center flex">
+                <div className="grow shrink basis-0 self-stretch pl-px flex-col justify-start items-center inline-flex">
+                  <div className="text-center text-green-600 text-opacity-50 text-4xl font-black font-['Font Awesome 5 Free'] leading-10"></div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className="h-16 pb-11 left-[10px] top-[79.84px] absolute flex-col justify-start items-start inline-flex">
+          <div className="self-stretch h-7 px-14 flex-col justify-start items-center flex">
+            <div className="text-center text-black text-base font-bold font-['Roboto'] leading-7">In-house Production</div>
+          </div>
+        </div>
+        <div className="h-20 pb-6 left-[10px] top-[121.69px] absolute flex-col justify-start items-start inline-flex">
+          <div className="self-stretch h-12 pr-0.5 flex-col justify-start items-start flex">
+            <div className="text-neutral-700 text-sm font-normal font-['Roboto'] leading-relaxed">capability with over 100 CNC machines<br/>including 5 axis, 3 axis and more.</div>
+          </div>
+        </div>
+      </div>
+    </div>
+    <div className="w-72 h-52 px-2.5 justify-start items-start flex">
+      <div className="w-64 h-52 relative rounded-tr-sm">
+        <div className="h-16 pb-5 left-[10px] top-[24px] absolute flex-col justify-start items-start inline-flex">
+          <div className="self-stretch h-12 px-24 pb-2.5 flex-col justify-start items-center flex">
+            <div className="justify-start items-start inline-flex">
+              <div className="w-10 h-10 justify-center items-center flex">
+                <div className="grow shrink basis-0 self-stretch pl-px flex-col justify-start items-center inline-flex">
+                  <div className="text-center text-green-600 text-opacity-50 text-4xl font-black font-['Font Awesome 5 Free'] leading-10"></div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className="h-16 pb-11 left-[10px] top-[79.84px] absolute flex-col justify-start items-start inline-flex">
+          <div className="self-stretch h-7 px-16 flex-col justify-start items-center flex">
+            <div className="text-center text-zinc-950 text-base font-bold font-['Roboto'] leading-7">Quick & Precision</div>
+          </div>
+        </div>
+        <div className="h-20 pb-6 left-[10px] top-[121.69px] absolute flex-col justify-start items-start inline-flex">
+          <div className="self-stretch h-12 pr-7 flex-col justify-start items-start flex">
+            <div className="text-neutral-700 text-sm font-normal font-['Roboto'] leading-relaxed">Turn around as fast as 1 day. Tight<br/>tolerance up to 0.01mm</div>
+          </div>
+        </div>
+      </div>
+    </div>
+    <div className="w-72 h-52 px-2.5 justify-start items-start flex">
+      <div className="w-64 h-52 relative rounded-tr-sm">
+        <div className="h-16 pb-5 left-[10px] top-[19px] absolute flex-col justify-start items-start inline-flex">
+          <div className="self-stretch h-12 px-24 pb-2.5 flex-col justify-start items-center flex">
+            <div className="justify-start items-start inline-flex">
+              <div className="w-10 h-10 justify-center items-center flex">
+                <div className="grow shrink basis-0 self-stretch pl-px flex-col justify-start items-center inline-flex">
+                  <div className="text-center text-green-600 text-opacity-50 text-4xl font-black font-['Font Awesome 5 Free'] leading-10"></div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className="h-16 pb-11 left-[10px] top-[79.84px] absolute flex-col justify-start items-start inline-flex">
+          <div className="self-stretch h-7 px-20 flex-col justify-start items-center flex">
+            <div className="text-center text-zinc-950 text-base font-bold font-['Roboto'] leading-7">7*24 Service</div>
+          </div>
+        </div>
+        <div className="h-20 pb-6 left-[10px] top-[121.69px] absolute flex-col justify-start items-start inline-flex">
+          <div className="self-stretch h-12 flex-col justify-start items-start flex">
+            <div className="text-neutral-700 text-sm font-normal font-['Roboto'] leading-relaxed">instant customer service. Expert quality<br/>control system.</div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+  <img className="w-96 h-96 left-[861.92px] top-[1402px] absolute origin-top-left rotate-[6.34deg]" src="https://via.placeholder.com/610x579" />
+  <div className="w-96 left-[150px] top-[2192px] absolute justify-start items-start inline-flex">
+    <div className="grow shrink basis-0 self-stretch p-2.5 flex-col justify-start items-start inline-flex">
+      <div className="self-stretch h-28 pb-16 flex-col justify-start items-start flex">
+      
