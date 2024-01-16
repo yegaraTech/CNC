5e585db5edcb9a8f41efa25910b7ab055025be06
JohnGirma <yohannesgirma543@gmial.com>
2024-01-16 11:23:15 +0300

    update

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -16,9 +16,9 @@ function Home() {
 
    
     <div data-aos="fade-right" data-aos-duration="1000" className="container mx-auto flex flex-col p-2 md:p-12 gap-6">
-        <h1 className="font-bold text-4xl lg:text-6xl  font-['Poppins'] ">Welcome to </h1>
-        <h1 className="font-bold text-4xl lg:text-6xl   font-['Poppins'] ">Markon Tech!</h1>
-        <p className=" text-stone-400 text-2xl font-semibold font-['Open Sans']">Cut-N-Create</p>
+        <h1 className="font-bold text-4xl lg:text-8xl  font-['Poppins'] ">Welcome to </h1>
+        <h1 className="font-bold text-4xl lg:text-8xl   font-['Poppins'] ">Markon Tech!</h1>
+        <p className=" text-stone-500 text-4xl font-semibold font-['Open Sans']">Cut-N-Create</p>
        {/* <button className="w-20 h-10 px-2 py-4mt-4 bg-black  text-white font-semibold font-['Inter'] rounded-md">BUY</button> */}
      </div>
     {/* <img className="lg:h-96 justify-center items-center self-center" src={Firstcnc} alt="First CNC" /> */}
